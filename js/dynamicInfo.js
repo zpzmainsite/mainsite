f35992833aa4e23893152cb067c7f6e5a5b60d8f
anycrane <anymajoy@gmail.com>
2014-09-29 10:27:40 +0800

    关注的动态

---
+++ js/dynamicInfo.js
@@ -0,0 +1,179 @@
+$(function () {
+	
+	dynamicLoader(dynamicLoader.scrollingLoader);
+	
+    console.log('user: ' + $.cookie('userID') + ' token: ' + ($.cookie('token') ? '(c)'+$.cookie('token') : '(o)'+global.test_token) );
+
+});
+
+var dynamicLoader = function (opt) {
+	
+	var makerendsRows = function (datas) {
+		var makeComments = function(data, row){
+			$rowHtml = $('<div class="trend_comments"/>');
+			$(data).each(function (i, j) {
+	        	var _row = makeCommentRow(j);
+	        	if(_row != null){
+	        		$rowHtml.append(_row);
+	        	}
+	        });
+			row.append($rowHtml);
+		};
+		
+		var makeCommentRow = function(data){
+			var timePassed = moment(data.createdTime).fromNow();
+	        var imgUrl = global.server + data.userImage;
+			return "<div class='comment-row'> \
+						<div class='left' style='width: 10%;'><img src='" + imgUrl + "'/></div> \
+						<div class='right' style='width: 90%;'> \
+							<div> \
+								<span class='nickname'>" + data.userName + "</span> \
+								<span class='time'>" + timePassed + "</span> \
+							</div> \
+							<div class='text'>" + data.commentContents + "</div> \
+						</div> \
+					<div class='clear'></div> \
+				</div>";
+		};
+		
+        var makeRow = function (data) {
+        	var actives = data.actives;
+        	var category = actives.category;
+        	var _el = null;
+        	if(category == 'Personal'){
+        		_el = person_row(actives);
+        	} else if(category == 'Product'){
+        		_el = product_row(actives);
+        	} else if(category == 'Project'){
+        		_el = project_row(actives);
+        	}
+        	if(_el != null){
+        		var comments = data.comments;
+        		if(comments.length > 0){
+        			makeComments(comments, _el);
+        		}
+        	}
+        	return _el;
+        };
+        
+        var person_row = function(actives){
+        	var el = $('<div class="row panel-shadow"> \
+    				<div class="trends friend-trends"> \
+    					<div> \
+    						<div class="left avatar"> \
+    							<img id="avatar_img" src=""> \
+    						</div> \
+    						<div class="right content"> \
+    							<div><span id="user_name"></span> <span id="create_time"></span></div> \
+    							<div>个人动态</div> \
+    							<div id="content"></div> \
+    						</div> \
+    						<div class="clear"></div> \
+    					</div> \
+    					<div> \
+    						<img id="image" src="" /> \
+    					</div> \
+    					<div class="tools_bar"> \
+    						<div>c</div> \
+                    	</div> \
+    				</div> \
+    			</div>');
+	        el.find('#avatar_img').attr("src", global.server + actives.avatarUrl);
+	        el.find('#user_name').text(actives.userName);
+	        el.find('#create_time').text(moment(actives.createdTime).fromNow());
+	        el.find('#content').text(actives.content);
+	        if(actives.imageLocation != undefined){
+	        	el.find("#image").attr("src", global.server + actives.imageLocation);
+	        } else {
+	        	el.find("#image").remove();
+	        }
+	        el.attr("ref", actives.id);
+	        return el;
+        };
+        
+        var product_row = function(actives){
+        	var el = $('<div class="row panel-shadow"> \
+				<div class="trends product-trends"> \
+					<div> \
+						<div class="left avatar"> \
+							<img id="avatar_img" src=""> \
+						</div> \
+						<div class="right content"> \
+							<div> \
+								<span id="title"></span> <span id="create_time"></span> \
+							</div> \
+							<div id="content"></div> \
+						</div> \
+						<div class="clear"></div> \
+					</div> \
+					<div class="tools_bar"> \
+						<div>c</div> \
+					</div> \
+				</div> \
+			</div>');
+	        el.find('#avatar_img').attr("src", global.server + actives.avatarUrl);
+	        el.find('#title').text(actives.title);
+	        el.find('#create_time').text(moment(actives.createdTime).fromNow());
+	        el.find('#content').text(actives.content);
+	        el.attr("ref", actives.id);
+	        return el;
+        };
+        
+        var project_row = function(actives){
+        	var el = $('<div class="row panel-shadow"> \
+				<div class="trends project-trends"> \
+					<div> \
+						<div class="left avatar"> \
+							<img id="avatar_img" src=""> \
+						</div> \
+						<div class="right content"> \
+							<div> \
+								<span id="title"></span> <span id="create_time"></span> \
+							</div> \
+							<div id="content"></div> \
+						</div> \
+						<div class="clear"></div> \
+					</div> \
+					<div class="tools_bar"> \
+						<div>c</div> \
+					</div> \
+				</div> \
+			</div>');
+	        el.find('#avatar_img').attr("src", global.server + actives.avatarUrl);
+	        el.find('#title').text(actives.title);
+	        el.find('#create_time').text(moment(actives.createdTime).fromNow());
+	        el.find('#content').text(actives.content);
+	        el.attr("ref", actives.id);
+	        return el;
+        };
+        
+        // $('.content dl dd').remove();
+        $(datas.data).each(function (i, j) {
+        	var _row = makeRow(j);
+        	if(_row != null){
+        		$('.trends_row').append(_row);
+        	}
+        });
+        //$('.endOfPage').show();
+    };
+    
+    var url = '/ActiveCenter/Actives?UserId=' + opt.userId + '&pageIndex=' + opt.index + '&pageSize=' + opt.pageSize;
+    
+    console.log(global.serviceUrl + url);
+    
+    
+	$.get(global.serviceUrl + url, function (msg) {
+        if (msg && msg.d && msg.d.status && msg.d.status.statusCode == global.status.success) {
+            makerendsRows(msg.d);
+        }
+    }).done(function (msg) {
+    	
+    });
+};
+
+dynamicLoader.scrollingLoader = {
+    index: 0,
+    pageSize: 12,
+    identify: 0,
+    userId: global.getUserId()
+};
---
+++ js/trendsInfo.js
@@ -0,0 +1,81 @@
+var tmp = { obj: {} };
+
+var readMultipleFiles = function (evt) {
+    //Retrieve all the files from the FileList object
+    var files = evt.target.files; 
+    		
+    if (files) {
+        for (var i = 0; i < files.length; i++) {
+        	if (files[i].type != 'image/jpeg') {
+		        var pop = new PopingView();
+		        pop.show({ text: files[i].name+" 文件类型错误，请上传JPG格式的文件！", parent: '.wrapper', view: "errorView", timeout: 4000, css: {top: '60px'} });
+        		continue;
+        	}
+        	if (files[i].size > 200000) {
+		        var pop = new PopingView();
+		        pop.show({ text: files[i].name+" 文件过大，请上传200K以内的文件！", parent: '.wrapper', view: "errorView", timeout: 4000, css: {top: '60px'} });
+        		continue;
+        	}
+            (function (i) {
+                var reader = new FileReader();
+                reader.onload = function (event) {
+                    tmp.obj = {
+                        "imgContent": event.target.result,
+                        "file": files[i]
+                    };
+                    trends.image = tmp.obj;
+                };
+                reader.readAsDataURL(files[i]);
+            })(i);
+        }
+    } else {
+		console.log("Failed to load files"); 
+    }
+};
+
+var trends = {
+	image : null,
+	sendActives : function(opt){
+		var content = $(".message-content").val();
+		var category = $("div[class='trends_tab active']").attr('tag');
+		var data = {
+			"EntityID"	: global.getUserId(),
+			"Category" : category,
+			"ActiveText" : content
+		};
+		if(trends.image != null){
+			data.PictureString = trends.image.imgContent.replace('data:image/jpeg;base64,','');
+			data.ActivePicture = trends.image.file.name;
+		}
+		if(content != ''){
+			trends.postData(data);
+		}
+	},
+	postData : function(data){
+		var url = "/ActiveCenter/SendActives";
+		$.ajax({
+			type : "post",
+			url : global.serviceUrl + url,
+			data : data,
+			dataType : "json",
+			success : function(msg) {
+				if (msg && msg.d && msg.d.status && msg.d.status.statusCode == global.status.success) {
+		            alert("cheng gong");
+		        }
+			}
+		});
+	}
+};
+
+
+$(function(){
+	$('.attach').hover(function () {
+        $(this).find('.cmps').addClass('cmps-hover');
+    }, function () {
+        $(this).find('.cmps').removeClass('cmps-hover');
+    }).on('click', function () {
+		var trigger = $(this).find('input:file')[0];
+		trigger.addEventListener('change', readMultipleFiles, false);
+        trigger.click();
+    });
+});
