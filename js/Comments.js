09fdb34fab29468b585598e693cc0bdd83feb499
anycrane <anymajoy@gmail.com>
2014-10-11 18:02:41 +0800

    add comments

--- js/Comments.js
+++ js/Comments.js
@@ -9,8 +9,8 @@ Comments.prototype.template = {
         var timePassed = moment(data.createdTime).fromNow();
         var imgUrl = global.server + data.userImage;
         return "<div class='comment-row'> \
-						<div class='left'><img src='" + imgUrl + "'/></div> \
-						<div class='left'> \
+						<div class='c_head'><img src='" + imgUrl + "'/></div> \
+						<div class='c_content'> \
 							<div> \
 								<span class='nickname'>" + data.userName + "</span> \
 								<span class='time'>" + timePassed + "</span> \
@@ -21,7 +21,7 @@ Comments.prototype.template = {
 				</div>";//更改了HTML
     },
     "commentForm": function () {
-        return "<img src='' /><input type='text' class='cmt' id='cmt'>";
+        return "<img src='' /><input type='text' class='cmt' id='cmt' />";
     }
 }
 
@@ -32,7 +32,6 @@ Comments.prototype.makeRows = function (data) {
                 '?entityType=' + this.opt.entityType + 
                 '&entityId=' + this.opt.entityId;
     $.get(url, function (msg) {
-    	msg = {"d":{"status":{"statusCode":1300},"data":[{"commentContents":"产品评论2！","entityType":"Product","entityId":"8891938d-0896-4d45-ae3a-934bf2ab063c","createdBy":"39123a9b-24a9-4ad8-a042-94ac4ca3106e","id":"b6b8c3c5-06be-4d73-bdc5-78cb4934e49e","createdTime":"2014-10-10T13:52:44.42","userName":null,"userImage":null},{"commentContents":"给个好评！","entityType":"Product","entityId":"8891938d-0896-4d45-ae3a-934bf2ab063c","createdBy":"39123a9b-24a9-4ad8-a042-94ac4ca3106e","id":"224ab060-5e3b-486d-99b6-147f1eb03750","createdTime":"2014-10-10T13:52:02.813","userName":null,"userImage":null},{"commentContents":"产品评论1！","entityType":"Product","entityId":"8891938d-0896-4d45-ae3a-934bf2ab063c","createdBy":"39123a9b-24a9-4ad8-a042-94ac4ca3106e","id":"bc6b96ad-707b-4809-a059-6ec59d78243f","createdTime":"2014-10-10T13:51:37.323","userName":null,"userImage":null}]}};
         if (msg && msg.d && msg.d.status && msg.d.status.statusCode == 1300) {
             $(msg.d.data).each(function () {
                 var rowHtml = _this.template.commentRow(this),
@@ -45,9 +44,41 @@ Comments.prototype.makeRows = function (data) {
 };
 
 Comments.prototype.makeForm = function () {
+	var _this = this;
     this.opt.form.append(this.template.commentForm());
+    
+    this.opt.form.find("#cmt").keydown(function(e){
+    	if(e.keyCode==13){
+    		_this.commitData();
+    	} 
+    }); 
 }
-
+Comments.prototype.commitData = function () {
+	var content = this.opt.form.find("#cmt").val();
+	var url = '/EntityComments/Add';
+	
+	var data = {};
+	data.EntityId = this.opt.entityId;
+	data.entityType = this.opt.entityType;
+	data.CommentContents = content;
+	data.CreatedBy = global.getUserId();
+	console.log(data);
+	
+	$.ajax({
+		type : "post",
+		url : global.serviceUrl + url,
+		data : data,
+		dataType : "json",
+		success : function(msg) {
+			console.log(msg);
+			if (msg && msg.d && msg.d.status && msg.d.status.statusCode == global.status.success) {
+	            alert("评论成功");
+	            this.opt.form.find("#cmt").val("");
+	        }
+		}
+	});
+	
+}
 Comments.prototype.alive = function () {
     // TODO
 }
