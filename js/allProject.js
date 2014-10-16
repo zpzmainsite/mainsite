fdfd1a9cb4bb766632e45b0aa8d852a5df291d23
anycrane <anymajoy@gmail.com>
2014-10-16 15:52:17 +0800

    all project focus

--- js/allProject.js
+++ js/allProject.js
@@ -80,7 +80,7 @@ var projectCardLoader = function (opt) {
                             <div class="mappoint" id=""></div> \
                             <p class="blue" id="district"></p> \
                             <p class="gray" id="province_city"></p> \
-                            <div class="focus"></div> \
+                            <div class="focus"><div class="off"></div></div> \
                         </dd>');
             el.find('#projectName').text(
                 data.projectName ?
@@ -106,9 +106,43 @@ var projectCardLoader = function (opt) {
                 location.href = surl;
             });
             el.find('.focus').on('click', function () {
-                console.log('focus focus');
-            })
+            	var dest = $(this).find("div");
+            	if(dest.hasClass("on")){
+            		var url = '/Projects/DeleteFocusProjects';
+            		var fId = $(el).data("fId");
+            		$.post(global.serviceUrl + url, {"DeletedBy":global.getUserId(),"ID":fId}, function(msg){
+                		if (msg && msg.d && msg.d.status && msg.d.status.statusCode == global.status.success) {
+                			dest.attr("class", "off");
+                		}
+                	});
+            	} else {
+            		var projectId = $(el).attr('ref');
+                	var url = '/Projects/AddProjectFocus';
+                	$.post(global.serviceUrl + url, {"UserId":global.getUserId(),"ProjectId":projectId}, function(msg){
+                		if (msg && msg.d && msg.d.status && msg.d.status.statusCode == global.status.success) {
+                			$(el).data("fId",msg.data.id);
+                			dest.attr("class", "on");
+                		}
+                	});
+            	}
+                return false;
+            });
             return el;
+        };
+        
+        var initFocus = function (){
+        	var url = '/Projects/FocusProjects?UserId=' + global.getUserId();
+        	$.get(global.serviceUrl + url, function (msg) {
+        		if (msg && msg.d && msg.d.status && msg.d.status.statusCode == global.status.success) {
+        			$(msg.d.data).each(function (i, j) {
+        				var dataId = j.id;
+        				var projectId = j.projectId;
+        	            var dest = $('.content dl').find("dd[ref='" + projectId+ "']");
+        	            dest.data("fId", dataId);
+        	            dest.find(".focus div").attr("class", "on");
+        	        });
+        		}
+            });
         };
 
         var pageCount = Math.round(datas.status.totalCount / global.scrollingLoader.pageSize);
@@ -122,6 +156,9 @@ var projectCardLoader = function (opt) {
         $(datas.data).each(function () {
             $('.content dl').append(card(this));
         });
+        
+        initFocus();
+        
         $('.endOfPage').show();
     }
 
